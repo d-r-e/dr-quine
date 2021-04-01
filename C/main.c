f6241a42e76ce2ce28de8375d621cbe31ffefc2d
d-r-e <egoz3ntrum@gmail.com>
2021-04-01 18:14:47 +0200

    d-r-e

--- C/main.c
+++ C/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
-int	main(void)
-{
-	puts("hol a");
-	return (0);
+void f(){};
+char*s="#include <stdio.h>%cvoid f(){};%cchar*s=%c#include <stdio.h>%ccvoid f(){};%ccchar*s=%cc#include <stdio.h";
+int main(){
+	f();
+	printf(s,10,10,34,37,37);
 }
