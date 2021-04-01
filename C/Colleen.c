ebbe1ca8f4813937033b996ca9740d4ea9426779
d-r-e <egoz3ntrum@gmail.com>
2021-04-01 19:27:08 +0200

    colleen.c

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-char *s = "%c%s%c;%cint main(){%c";
+char *s = "#include <stdio.h>%cchar *s = %c%s%c;%cint main(){%c%cprintf(s,10,34,s,34,10,10,9,10,10);%c}%c";
 int main(){
-	printf(s, 34,s,34,10,10);
+		printf(s,10,34,s,34,10,10,9,10,10);
 }
+
