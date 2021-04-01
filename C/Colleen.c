9cbbb4590f3e36bad12e1356eae45f455279f69d
d-r-e <egoz3ntrum@gmail.com>
2021-04-01 20:02:29 +0200

    d-r-e

--- C/Colleen.c
+++ C/Colleen.c
@@ -3,7 +3,7 @@
    darodrig
 */
 void f(){}
-char *s = "#include <stdio.h>%c/*%c   darodrig%c*/%cvoid f(){}%cchar *s = %c%s%c;%cint main(){%c/*%c   darodrig%c*/%c%cf();%c%cprintf(s,10,10,10,10,10,34,s,34,10,10,9,10,9,10,10);%c}%c";
+char *s = "#include <stdio.h>%c/*%c   darodrig%c*/%cvoid f(){}%cchar *s = %c%s%c;%cint main(){%c/*%c   darodrig%c*/%c%cf();%c%cprintf(s,10,10,10,10,10,34,s,34,10,10,10,10,10,9,10,9,10,10);%c}%c";
 int main(){
 /*
    darodrig
