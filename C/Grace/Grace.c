422d69c1bb85fc273231c22d753cdf474bd26846
d-r-e <egoz3ntrum@gmail.com>
2021-04-02 11:36:24 +0200

    three dfines

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<fcntl.h>
-#define GRACE "#include<stdio.h>%c#include<unistd.h>%c#include<fcntl.h>%c#define GRACE %c%s%c%c#define quine(x) int main(){int f=open(%cGrace_kid.c%c, O_WRONLY|O_TRUNC, 644);dprintf(f,x,10,10,10,34,x,34,10,34,34,10,10,9,10,10,10);close(f);}%c/*%c%cdarodrig%c*/%cquine(GRACE)%c"
-#define quine(x) int main(){int f=open("Grace_kid.c", O_WRONLY|O_TRUNC, 644);dprintf(f,x,10,10,10,34,x,34,10,34,34,10,10,9,10,10,10);close(f);}
+#define START main
+#define GRACE "#include<stdio.h>%c#include<unistd.h>%c#include<fcntl.h>%c#define START main%c#define GRACE %c%s%c%c#define quine(x) int START(){int f=open(%cGrace_kid.c%c, O_WRONLY|O_TRUNC, 644);dprintf(f,x,10,10,10,10,34,x,34,10,34,34,10,10,9,10,10,10);close(f);}%c/*%c%cdarodrig%c*/%cquine(GRACE)%c"
+#define quine(x) int START(){int f=open("Grace_kid.c", O_WRONLY|O_TRUNC, 644);dprintf(f,x,10,10,10,10,34,x,34,10,34,34,10,10,9,10,10,10);close(f);}
 /*
 	darodrig
 */
