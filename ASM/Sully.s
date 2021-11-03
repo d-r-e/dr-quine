7510b7c25fed28cb31269ce8547d32848354bf93
David R <egoz3ntrum@gmail.com>
2021-11-03 13:58:02 +0100

    casi Sully

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,37 +1,66 @@
+%define NUMBER 9
 section .data
-quine: db `section .data%c%cquine: db %c%s%3$c%1$csection .text%1$c%2$cextern _printf%1$c%2$cglobal _main%1$c_main:%1$c%2$cpush rbx%1$c%2$cmov rdi, quine%1$c%2$c%mov rsi, 0x0a ; nl %%1$c%1$c%2$cmov rdx, 0x09 ; tab %%2$c%1$c%2$cmov rcx, 0x60 ; inverted apostrophe %%3$c%1$c%2$cmov r8, quine ; %%1$s%1$c%2$cmov r9, 0x60%1$c%2$cxor rax,rax%1$c%2$ccall _printf%1$c%2$cpop rbp%1$c%2$cmov rax, 0%1$c%2$cret%1$c`
-echo: db `echo cualquier cosa`,0
-number: db 5
-filename: db `Sully_6.s`,0
+quine: db ``
+number: db 0x00000005
+filename: db `\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0`
+fmt: db `Sully_%d.s`, 0
+compile: db `nasm -fmacho64 Sully_%1$d.s -o sully.o && gcc -m64 -c sully.o -o Sully_%1$d && ./Sully_%1$d`, 0xa, 00
 fd: db 0
-buff: db `\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0`
+buff: db `\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0`
 section .text
 extern _printf
 extern _system
-extern _strcpy
-extern _dprintf
+extern _sprintf
 extern _open
+extern _close
 global _main
-_main:
+build_filename:
 push rbx
-mov r10, number
-dec r10
-mov rdi, 2
-mov rsi, echo
-mov rax, 0
-call _dprintf
+mov rdi, filename
+mov rsi, fmt
+mov rdx, NUMBER - 1
+cmp rdx, 0
+je exit
+call _sprintf
+cmp rax, 0
+jle err
+mov rax, rdi
 pop rbx
 ret
-drquine:
+write_quine:
 push rbx
-mov rdi, quine
-mov rsi, 0x0a ; nl %1$c
-mov rdx, 0x09 ; tab %2$c
-mov rcx, 0x60 ; inverted apostrophe %3$c
-mov r8, quine ; %1$s
-mov r9, 0x60
-xor rax,rax
-; call _printf
+mov rdi, filename
+mov rsi, 0x601
+mov rdx, 644o
+call _open
+mov r14, rax ;fd
+mov rdi, r14
+call _close
+jc err
 pop rbx
+ret
+build_command:
+push rbx
+call build_filename
+mov rdi, rax
+call write_quine
+mov rdi, buff
+mov rsi, compile
+mov rdx, NUMBER - 1
+call _sprintf
+mov rdi, buff
+call _printf
+pop rbx
+ret
+_main:
+push rbx
+call build_command
+
+pop rbx
+ret
+err:
+mov rax, -1
+ret
+exit:
 mov rax, 0
 ret
